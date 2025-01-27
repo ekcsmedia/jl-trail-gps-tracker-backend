331872ec5ce2a0a833febdfc57a45a20fb2c34d3
santhosh <santosh.kumar@simplifyvms.com>
2025-01-27 17:12:41 +0530

    client n driver CRUD Completed

--- src/infrastructure/controllers/client.controller.ts
+++ src/infrastructure/controllers/client.controller.ts
@@ -10,19 +10,19 @@ export async function createClient(req: FastifyRequest, reply: FastifyReply) {
         const clientData = req.body;
         const { id, ...clientWithoutId } = clientData as ClientEntity;
         const client = await clientUseCases.createClient(clientWithoutId);
-        return reply.status(201).send(client);
+        return reply.status(201).send({message:"client data create successfully", payload: client});
     }
 
 export async function getClient(req: FastifyRequest, reply: FastifyReply) {
         const { id } = req.params as { id: string };
         const client = await clientUseCases.getClient(id);
         if (!client) return reply.status(404).send({ message: 'Client not found' });
-        return reply.status(200).send(client);
+        return reply.status(200).send({message:"client data fetched successfully", payload: client});
     }
 
 export async function getAllClients(req: FastifyRequest, reply: FastifyReply) {
         const clients = await clientUseCases.getAllClients();
-        return reply.status(200).send(clients);
+        return reply.status(200).send({message:"client data fetched successfully", payload: clients});
     }
 
 export async function updateClient(req: FastifyRequest, reply: FastifyReply) {
@@ -30,7 +30,7 @@ export async function updateClient(req: FastifyRequest, reply: FastifyReply) {
         const clientData = req.body;
         const updatedClient = await clientUseCases.updateClient(id, clientData);
         if (!updatedClient) return reply.status(404).send({ message: 'Client not found' });
-        return reply.status(200).send(updatedClient);
+        return reply.status(200).send({message:"client data updated successfully", payload: updatedClient});
     }
 
 export async function deleteClient(req: FastifyRequest, reply: FastifyReply) {
