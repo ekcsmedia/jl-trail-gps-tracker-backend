6147e94f2e5b3f5a2dc30c6b8902cc441fe5f72a
santhosh <santosh.kumar@simplifyvms.com>
2025-04-01 00:28:23 +0530

    get all end points redefined

--- src/infrastructure/routes/driver.routes.ts
+++ src/infrastructure/routes/driver.routes.ts
@@ -9,7 +9,7 @@ import {
 
 export default async function driverRoutes(app: FastifyInstance) {
     app.post('/drivers', createDriverHandler);
-    app.get('/drivers', getAllDriversHandler);
+    app.get('/drivers-all', getAllDriversHandler);
     app.get('/drivers/:id', getDriver);
     app.put('/drivers/:id', updateDriver);
     app.delete('/drivers/:id', deleteDriver);
