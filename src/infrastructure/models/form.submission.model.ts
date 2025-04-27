05cf873461b724cf9e3b6ed4826bf403de24c44b
santhosh <santosh.kumar@simplifyvms.com>
2025-04-27 21:16:28 +0530

    export issue fixed

--- src/infrastructure/models/form.submission.model.ts
+++ src/infrastructure/models/form.submission.model.ts
@@ -7,33 +7,33 @@ export class FormSubmission extends Model {
     @Column({ primaryKey: true, type: DataType.INTEGER, autoIncrement: true })
     id!: number;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     location!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     date!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     masterDriverName!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     empCode!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     mobileNo!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     customerDriverName!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     customerMobileNo!: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
+    @Column({ type: DataType.STRING, allowNull: true })
     licenseNo!: string;
 
-    @Column({ type: DataType.JSON, allowNull: false })
+    @Column({ type: DataType.JSON, allowNull: true })
     vehicleDetails!: any[];
 
-    @Column({ type: DataType.JSON, allowNull: false })
+    @Column({ type: DataType.JSON, allowNull: true })
     competitorData!: any[];
 }
